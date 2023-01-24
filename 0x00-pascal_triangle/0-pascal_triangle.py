d6a7a4cd05aa58f0995846774ae85d94d812f77f
BOSTONE069 <bostoneochieng69@gmail.com>
2023-01-24 11:44:07 -0500

    include the necessary whitespaces

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,9 +11,9 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     triangle = [[1]]
-    for i in range(1,n):
+    for i in range(1, n):
         row = [1]
-        for j in range(1,i):
+        for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)

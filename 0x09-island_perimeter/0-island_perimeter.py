6aaf6e2557520f430499b615ba9d1caf85c4ca9b
BOSTONE069 <bostoneochieng69@gmail.com>
2023-03-29 12:45:46 -0400

    added the island perimeter program and the readnme file

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" Function to find perimeter of an island """
+
+
+def island_perimeter(grid):
+    """
+    Input: List of Lists
+    Returns: Perimeter of the island
+    """
+    if not grid or not grid[0]:  # if the grid is empty or the first row is empty, return 0
+        return 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
